a1be0b59390f10b6856514e68e63b2c81026e5a9
tzils <tzil.saar@gmail.com>
2019-10-03 15:51:34 +0200

    Updates

---
+++ precache-manifest.e8154be2d48a1746e5f9b7dc20c2526d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85423e88916f72094a9d0cfeaf984c8e",
+    "url": "/portcv/index.html"
+  },
+  {
+    "revision": "eb6e5ee0c0a004b03999",
+    "url": "/portcv/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7fbd16b27b875ad66076",
+    "url": "/portcv/static/js/2.a3cc1d30.chunk.js"
+  },
+  {
+    "revision": "eb6e5ee0c0a004b03999",
+    "url": "/portcv/static/js/main.f5e45af7.chunk.js"
+  },
+  {
+    "revision": "c014307039311cc072f7",
+    "url": "/portcv/static/js/runtime-main.a9ff19bd.js"
+  }
+]);
