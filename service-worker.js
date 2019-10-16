5ead6fbfb36e73a10cae03a58e3b17043e857e75
tzils <tzil.saar@gmail.com>
2019-10-16 14:02:46 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portcv/precache-manifest.35063eb64996fed9a53cd171d4070b96.js"
+  "/portcv/precache-manifest.4393457a642cef4a9110a0708f45b438.js"
 );
 
 self.addEventListener('message', (event) => {
